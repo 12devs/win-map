e7e1f7f25ca19518721825bd50015342472380cc
DmitriKul <KulDmitriBy@gmail.com>
2018-10-17 10:44:43 +0300

    update backend

--- backend/src/routes/points.js
+++ backend/src/routes/points.js
@@ -6,5 +6,6 @@ const route = express.Router();
 route.post('/save', point.save);
 route.post('/withData', point.withData);
 route.post('/delete', point.deletePoint);
+route.post('/move', point.movePoint);
 
 export default route;
