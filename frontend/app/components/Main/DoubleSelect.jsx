79dde2001b908031c3aad96b71e913bb2b2464ec
Andrei Yunkevich <andreyha.by@gmail.com>
2018-10-19 16:53:46 +0300

    fix NotificationSettings

--- frontend/app/components/Main/DoubleSelect.jsx
+++ frontend/app/components/Main/DoubleSelect.jsx
@@ -7,40 +7,22 @@ import makeAnimated from 'react-select/lib/animated';
 class DoubleSelect extends React.Component {
   constructor(props) {
     super(props);
-    this.pointHandleChange = this.pointHandleChange.bind(this);
-    this.dangerHandleChange = this.dangerHandleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.delSetting = this.delSetting.bind(this);
   }
 
-  pointHandleChange = (newValue) => {
+  handleChange = (newValue, type) => {
     let points = this.props.notificationSettings.toJS();
     const point = points.find(x => x.arrId === this.props.point.arrId);
-    console.log('point', point);
     if (point) {
-      point.place = newValue;
+      point[type] = newValue;
       points = this.props.notificationSettings.toJS().filter(x => !(x.arrId === this.props.point.arrId));
       points.push(point);
       this.props.updateNotificationSettings(points);
 
     }
     else {
-      points.push({arrId: this.props.point.arrId, place: newValue});
-      this.props.updateNotificationSettings(points);
-    }
-  };
-
-  dangerHandleChange = (newValue) => {
-    let points = this.props.notificationSettings.toJS();
-    const point = points.find(x => x.arrId === this.props.point.arrId);
-    if (point) {
-      point.danger = newValue;
-      points = this.props.notificationSettings.toJS().filter(x => !(x.arrId === this.props.point.arrId));
-      points.push(point);
-      this.props.updateNotificationSettings(points);
-
-    }
-    else {
-      points.push({arrId: this.props.point.arrId, danger: newValue});
+      points.push({arrId: this.props.point.arrId, [type]: newValue});
       this.props.updateNotificationSettings(points);
     }
   };
@@ -51,42 +33,30 @@ class DoubleSelect extends React.Component {
   };
 
   render() {
-    const danger = this.props.points
-      .filter(el => el.get('type') === 'Danger')
+    const danger = this.props.dangers
       .map((point) => {
           return {value: point.get('id'), label: point.get('name')};
         }
       );
 
-    const place = this.props.points
-      .filter(el => el.get('type') === 'My Place')
+    const place = this.props.places
       .map((point) => {
           return {value: point.get('id'), label: point.get('name')};
         }
       );
-
-    const place3 = place.filter(x => this.props.notificationSettings.toJS().findIndex(el => {
-      if (el.place) {
-        console.log(x.value);
-        return el.place.value === x.value;
-      }
-      return false;
-    }))
-
-    console.log(place3.toJS());
 
     return (
       <div>
         <Select
           isClearable
           components={makeAnimated()}
-          onChange={this.pointHandleChange}
-          options={place3}
+          onChange={(e) => this.handleChange(e, 'place')}
+          options={place}
         />
         <Select
           closeMenuOnSelect={false}
           components={makeAnimated()}
-          onChange={this.dangerHandleChange}
+          onChange={(e) => this.handleChange(e, 'danger')}
           isMulti
           options={danger}
         />
@@ -98,7 +68,8 @@ class DoubleSelect extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    points: state.get('points'),
+    places: state.get('places'),
+    dangers: state.get('dangers'),
     notificationSettings: state.get('notificationSettings')
   };
 }
--- frontend/app/components/Main/markers/UserPlace.jsx
+++ frontend/app/components/Main/markers/UserPlace.jsx
@@ -52,10 +52,6 @@ class UserPlace extends React.Component {
         }}
         position={[this.props.point.lat, this.props.point.lng]}
         icon={blueIcon}>
-        {this.props.actionType === 'Add' ?
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup> : null}
       </Marker>
     );
   }
