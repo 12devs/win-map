712c3c0cc1d67679a1286f45f1256f647b0eaa7d
DmitriKul <KulDmitriBy@gmail.com>
2018-10-30 13:11:52 +0300

    start login

--- backend/src/routes/publicRouts.js
+++ backend/src/routes/publicRouts.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import security from '../controllers/security';
+
 const route = express.Router();
 
-route.post('/register',  security.register);
-route.post('/login',  security.login);
+route.post('/register', security.register);
+route.post('/login', security.login);
+route.get('/test', (req, res) => {
+  res.status(200).json({ message: 'OK' })
+});
 
 export default route;
