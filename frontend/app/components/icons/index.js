794a171aced5461cdd395f3bf1b8f152cd1667fc
Andrei Yunkevich <andreyha.by@gmail.com>
2018-11-01 11:10:02 +0300

    update marker icons for frontend

--- frontend/app/components/icons/index.js
+++ frontend/app/components/icons/index.js
@@ -1,11 +1,11 @@
 import L from 'leaflet';
-import markerBlue from '../../assets/img/map_blue.png';
-import markerRed from '../../assets/img/map_red.png';
+import markerBlue from '../../assets/img/point_blue.png';
+import markerRed from '../../assets/img/point_red.png';
 
 export const redIcon = L.icon({
   iconUrl: markerRed,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
+  iconSize: [29, 40],
+  iconAnchor: [14.5, 40],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
   shadowSize: [41, 41]
@@ -13,8 +13,8 @@ export const redIcon = L.icon({
 
 export const blueIcon = L.icon({
   iconUrl: markerBlue,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
+  iconSize: [29, 41],
+  iconAnchor: [14.5, 40],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
   shadowSize: [41, 41]
