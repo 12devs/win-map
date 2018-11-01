92bd40e23dd6c3191540a28dc2bd0b74d47f456b
Andrei Yunkevich <andreyha.by@gmail.com>
2018-11-01 15:53:10 +0300

    del point

--- mobile/PointSettings.js
+++ mobile/PointSettings.js
@@ -7,14 +7,36 @@ import {
   StyleSheet,
   AsyncStorage,
   Modal,
-  Alert,
-  TouchableHighlight
+  Button
 } from 'react-native';
 import actions from "./actions";
 import { connect } from "react-redux";
 import WindRoseChart from './WindRoseChart';
+import services from './services';
 
 class PointSettings extends Component {
+  constructor() {
+    super();
+    this.delMarker = this.delMarker.bind(this);
+  }
+
+  delMarker() {
+    const { point, type } = this.props.info;
+    const { id } = point;
+    return services.deletePoint({
+      [type]: { id },
+    })
+      .then(res => {
+        if (type === 'place') {
+          const places = this.props.places.filter(el => !(el.id === id));
+          this.props.updateReduxState({ places });
+        }
+        if (type === 'danger') {
+          const dangers = this.props.dangers.filter(el => !(el.id === id));
+          this.props.updateReduxState({ dangers });
+        }
+      });
+  };
 
   render() {
     const { point, type } = this.props.info;
@@ -39,6 +61,14 @@ class PointSettings extends Component {
           <Text>Lat: {point.lat}</Text>
           <Text>Lng: {point.lng}</Text>
           <WindRoseChart stationId={point.station_id}/>
+          <Button
+            title='Remove point'
+            onPress={() => {
+              this.delMarker()
+                .then(() => {
+                  this.props.updateReduxState({ info: { point: null, type: null } });
+                });
+            }}/>
         </View>
       </Modal>
     );
@@ -47,6 +77,15 @@ class PointSettings extends Component {
 
 function mapStateToProps(state) {
   return {
+    places: state.get('places'),
+    savePointSettings: state.get('savePointSettings'),
+    dangers: state.get('dangers'),
+    stations: state.get('stations'),
+    stationsData: state.get('stationsData'),
+    markerType: state.get('markerType'),
+    viewType: state.get('viewType'),
+    actionType: state.get('actionType'),
+    isSavePointSettingsOpen: state.get('isSavePointSettingsOpen'),
     info: state.get('info'),
   };
 }
