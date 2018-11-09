f483e1893756f6b8993d28ef429e180fac8abdf4
DmitriKul <KulDmitriBy@gmail.com>
2018-11-09 15:34:25 +0300

    init onesignal

--- mobile/android/app/src/main/java/com/emptyprojecttemplate/MainApplication.java
+++ mobile/android/app/src/main/java/com/emptyprojecttemplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.emptyprojecttemplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage()
+          new ReactNativeOneSignalPackage(),
+          new MapsPackage()
       );
     }
 
