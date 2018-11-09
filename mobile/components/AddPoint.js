f026e53a8f73dcd11dc1765c1e549128a6379aeb
Andrei Yunkevich <andreyha.by@gmail.com>
2018-11-09 17:22:56 +0300

    add point style

--- mobile/components/AddPoint.js
+++ mobile/components/AddPoint.js
@@ -6,7 +6,7 @@ import {
   StyleSheet,
   TextInput,
   View,
-  Modal
+  Modal, Text
 } from 'react-native';
 import { Header, Button } from 'react-native-elements';
 
@@ -18,56 +18,64 @@ class AddPoint extends React.Component {
       markerType: '',
     };
     this.addMarker = this.addMarker.bind(this);
+    this.markerType = this.markerType.bind(this);
   }
 
-  addMarker(markerType) {
-    const { latlng } = this.props.savePointSettings;
-    const { name } = this.state;
+   addMarker() {
+     const { latlng } = this.props.savePointSettings;
+     const { markerType } = this.props;
+     const { name } = this.state;
 
-    if (!name) {
-      return;
-    }
+     if (!name) {
+       return;
+     }
 
-    let key;
-    if (markerType === 'Danger') {
-      key = 'danger';
-    } else {
-      key = 'place';
-    }
+     let key;
+     if (markerType === 'Danger') {
+       key = 'danger';
+     } else {
+       key = 'place';
+     }
 
-    let lngCorrect = latlng.lng;
-    lngCorrect = lngCorrect % 360;
-    if (lngCorrect > 180) {
-      lngCorrect -= 360;
-    }
-    if (lngCorrect < -180) {
-      lngCorrect += 360;
-    }
-    return services.savePoint({
-      [key]: {
-        lat: latlng.lat,
-        lng: lngCorrect,
-        name
-      },
-      stations: [...this.props.stations]
-    })
-      .then(res => {
-        const { danger, place } = res;
-        let { places, dangers, stationsData, stations } = this.props;
-        stationsData = { ...stationsData, ...res.stationsData };
-        if (danger) {
-          dangers.push(danger);
-        }
-        if (place) {
-          places.push(place);
-        }
-        stations.push(...Object.keys((res.stationsData || {})));
-        this.props.updateReduxState({ places, dangers, stationsData, stations, savePointSettings: { show: false } });
-      });
+     let lngCorrect = latlng.lng;
+     lngCorrect = lngCorrect % 360;
+     if (lngCorrect > 180) {
+       lngCorrect -= 360;
+     }
+     if (lngCorrect < -180) {
+       lngCorrect += 360;
+     }
+     return services.savePoint({
+       [key]: {
+         lat: latlng.lat,
+         lng: lngCorrect,
+         name
+       },
+       stations: [...this.props.stations]
+     })
+       .then(res => {
+         const { danger, place } = res;
+         let { places, dangers, stationsData, stations } = this.props;
+         stationsData = { ...stationsData, ...res.stationsData };
+         if (danger) {
+           dangers.push(danger);
+         }
+         if (place) {
+           places.push(place);
+         }
+         stations.push(...Object.keys((res.stationsData || {})));
+         this.props.updateReduxState({ places, dangers, stationsData, stations, savePointSettings: { show: false } });
+       });
+   };
+
+  markerType(markerType) {
+    this.props.updateReduxState({ markerType: markerType });
   };
 
   render() {
     const { show } = this.props.savePointSettings;
+    const { markerType } = this.props;
+    console.log(markerType);
 
     if (!show) {
       return null;
@@ -87,39 +95,49 @@ class AddPoint extends React.Component {
               this.props.updateReduxState({ savePointSettings: { show: false } });
             }
           }}
+          rightComponent={{
+            icon: 'done', color: '#fff',
+            onPress: () => {
+              this.addMarker();
+            }
+          }}
           centerComponent={{ text: 'Add Point', style: { color: '#fff', fontSize: 20 } }}
           outerContainerStyles={{ backgroundColor: '#3D6DCC' }}
         />
         <View style={{ height: '100%', backgroundColor: '#fff' }}>
           <View>
+            <Text style={{ textAlign: 'center', marginTop: 10 }}>Enter marker name:</Text>
             <TextInput style={styles.input}
                        allowFontScaling={true}
                        underlineColorAndroid="transparent"
-                       placeholder="Point Name"
+                       placeholder="Marker Name"
                        placeholderTextColor="#3D6DCC"
                        autoCapitalize="none"
                        onChangeText={(e) => {
                          this.setState({ name: e });
                        }}/>
+            <Text style={{ textAlign: 'center' }}>Choose marker type:</Text>
             <Button
-              containerViewStyle={{ margin: 10 }}
-              backgroundColor={'#3D6DCC'}
+              containerViewStyle={{ margin: 10, borderWidth: 1, borderColor: '#3D6DCC' }}
+              backgroundColor={markerType === 'Danger' ? '#fff' : '#3D6DCC'}
               large
               borderRadius={50}
-              icon={{ name: 'home' }}
+              icon={markerType === 'Danger' ? { name: 'home', color: '#3D6DCC' } : { name: 'home', color: '#fff' }}
               title='My Place'
+              color={markerType === 'Danger' ? '#3D6DCC' : '#fff'}
               onPress={() => {
-                this.addMarker('My Place');
+                this.markerType('My Place');
               }}/>
             <Button
-              containerViewStyle={{ margin: 10 }}
-              backgroundColor={'red'}
+              containerViewStyle={{ margin: 10, borderWidth: 1, borderColor: 'red' }}
+              backgroundColor={markerType !== 'Danger' ? '#fff' : 'red'}
               large
               borderRadius={50}
-              icon={{ name: 'error' }}
+              icon={markerType !== 'Danger' ? { name: 'error', color: 'red' } : { name: 'error', color: '#fff' }}
+              color={markerType !== 'Danger' ? 'red' : '#fff'}
               title='Danger'
               onPress={() => {
-                this.addMarker('Danger');
+                this.markerType('Danger');
               }}/>
           </View>
         </View>
@@ -144,24 +162,10 @@ function mapStateToProps(state) {
 export default connect(mapStateToProps, actions)(AddPoint);
 
 const styles = StyleSheet.create({
-  content: {
-    flex: 1,
-    justifyContent: "center"
-  },
-  modal: {
-    backgroundColor: 'rgba(0,0,0,.8)',
-    position: 'absolute',
-    width: 10,
-    height: 10,
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0
-  },
   input: {
     marginLeft: 20,
     marginRight: 20,
-    marginTop: 20,
+    marginTop: 10,
     marginBottom: 10,
     height: 60,
     paddingLeft: 20,
@@ -169,6 +173,5 @@ const styles = StyleSheet.create({
     borderColor: '#3D6DCC',
     borderWidth: 1,
     borderRadius: 50
-    // flex:1
   },
 });
