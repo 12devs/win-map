d66aa0eca25e751a7b7fd46c4279d1cfa926155a
Andrei Yunkevich <andreyha.by@gmail.com>
2018-11-21 16:38:14 +0300

    style login, register, change password

--- mobile/components/Register.js
+++ mobile/components/Register.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 import services from '../services/index';
-import { Button } from 'react-native-elements';
+import { Button, Icon } from 'react-native-elements';
+
+const { width, height } = Dimensions.get('window');
 
 class Register extends Component {
   state = {
@@ -20,7 +22,7 @@ class Register extends Component {
         .then(res => {
           const { message, error } = res;
           if (message !== 'OK') {
-            this.setState({ error});
+            this.setState({ error });
           }
           else
             return this.props.navigation.navigate('Login');
@@ -36,46 +38,87 @@ class Register extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <TextInput style={styles.input}
-                   underlineColorAndroid="transparent"
-                   placeholder="Username"
-                   placeholderTextColor="#3D6DCC"
-                   autoCapitalize="none"
-                   value={this.state.login}
-                   onChangeText={(login) => this.setState({ login })}/>
-        <TextInput style={styles.input}
-                   underlineColorAndroid="transparent"
-                   secureTextEntry={true}
-                   placeholder="Password"
-                   placeholderTextColor="#3D6DCC"
-                   autoCapitalize="none"
-                   value={this.state.password}
-                   onChangeText={(password) => this.setState({ password })}/>
-        <TextInput style={styles.input}
-                   underlineColorAndroid="transparent"
-                   secureTextEntry={true}
-                   placeholder="Repeat Password"
-                   placeholderTextColor="#3D6DCC"
-                   autoCapitalize="none"
-                   value={this.state.repeatPassword}
-                   onChangeText={(repeatPassword) => this.setState({ repeatPassword })}/>
-        <TextInput style={styles.input}
-                   underlineColorAndroid="transparent"
-                   placeholder="email"
-                   placeholderTextColor="#3D6DCC"
-                   autoCapitalize="none"
-                   value={this.state.email}
-                   onChangeText={(email) => this.setState({ email })}/>
-        {this.state.error ? <Text style={{ textAlign: 'center', color: 'red' }}> {this.state.error}</Text> : null}
-        <Button
-          containerViewStyle={{ margin: 10, borderWidth: 1, borderColor: '#3D6DCC' }}
-          backgroundColor={'#3D6DCC'}
-          large
-          borderRadius={50}
-          title='Register'
-          color={'#fff'}
-          onPress={this.register}/>
+      <View style={styles.mainContainer}>
+        <View style={styles.container}>
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+            <TextInput style={styles.input}
+                       underlineColorAndroid="transparent"
+                       placeholder="email"
+                       placeholderTextColor="#3D6DCC"
+                       autoCapitalize="none"
+                       value={this.state.email}
+                       onChangeText={(email) => this.setState({ email })}/>
+            <View style={styles.iconContainer}>
+              <Icon name='mail-outline' color='#3D6DCC'/>
+            </View>
+          </View>
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+            <TextInput style={styles.input}
+                       underlineColorAndroid="transparent"
+                       placeholder="Username"
+                       placeholderTextColor="#3D6DCC"
+                       autoCapitalize="none"
+                       value={this.state.login}
+                       onChangeText={(login) => this.setState({ login })}/>
+            <View style={styles.iconContainer}>
+              <Icon name='perm-identity' color='#3D6DCC'/>
+            </View>
+          </View>
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+            <TextInput style={styles.input}
+                       underlineColorAndroid="transparent"
+                       secureTextEntry={true}
+                       placeholder="Password"
+                       placeholderTextColor="#3D6DCC"
+                       autoCapitalize="none"
+                       value={this.state.password}
+                       onChangeText={(password) => this.setState({ password })}/>
+            <View style={styles.iconContainer}>
+              <Icon name='lock-outline' color='#3D6DCC'/>
+            </View>
+          </View>
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}>
+            <TextInput style={styles.input}
+                       underlineColorAndroid="transparent"
+                       secureTextEntry={true}
+                       placeholder="Repeat Password"
+                       placeholderTextColor="#3D6DCC"
+                       autoCapitalize="none"
+                       value={this.state.repeatPassword}
+                       onChangeText={(repeatPassword) => this.setState({ repeatPassword })}/>
+            <View style={styles.iconContainer}>
+              <Icon name='lock-outline' color='#3D6DCC'/>
+            </View>
+          </View>
+          {this.state.error ? <Text style={{ textAlign: 'center', color: 'red' }}> {this.state.error}</Text> : null}
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.navigate('Login');
+            }}
+          >
+            <Text style={styles.textContainer}>Do have an account? You can login here.</Text>
+          </TouchableOpacity>
+          <Button
+            containerViewStyle={styles.buttonContainer}
+            backgroundColor={'#3D6DCC'}
+            // large
+            borderRadius={50}
+            title='Register'
+            color={'#fff'}
+            onPress={this.register}/>
+        </View>
       </View>
     );
   }
@@ -84,22 +127,50 @@ class Register extends Component {
 export default Register;
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     marginTop: 10,
     flexDirection: 'column',
     alignItems: 'stretch',
-    flex: 1,
+    flex: 2,
+  },
+  container: {
+    paddingTop: 30,
+    paddingBottom: 30,
+    backgroundColor: '#fff',
+    margin: 10,
+    elevation: 5
+  },
+  iconContainer: {
+    borderBottomColor: '#3D6DCC',
+    borderBottomWidth: 1,
+    width: 40,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
   },
   input: {
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 10,
+    // marginTop: 10,
     marginBottom: 10,
     height: 60,
-    paddingLeft: 20,
-    paddingRight: 20,
-    borderColor: '#3D6DCC',
-    borderWidth: 1,
-    borderRadius: 50
+    borderBottomColor: '#3D6DCC',
+    borderBottomWidth: 1,
+    width: width / 1.3,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  textContainer: {
+    textAlign: 'center',
+    textAlignVertical: "center",
+    color: '#525966',
+    padding: 10,
+  },
+  buttonContainer: {
+    marginTop: 15,
+    marginBottom: 10,
+    marginLeft: width * 0.15,
+    marginRight: width * 0.15,
+    borderWidth: 1,
+    borderColor: '#3D6DCC'
+  }
 });
