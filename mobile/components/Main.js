843acad0d5c5f00e0fcaeb34ada3585d0e14199b
Andrei Yunkevich <andreyha.by@gmail.com>
2018-11-26 11:10:16 +0300

    internet connection

--- mobile/components/Main.js
+++ mobile/components/Main.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, NetInfo, Dimensions, Text } from 'react-native';
 import actions from '../actions/index';
 import { connect } from "react-redux";
 import Map from './Map';
 import services from "../services/index";
 import { calcMapRegionAll } from '../utils';
 
+const { width } = Dimensions.get('window');
+
+function MiniOfflineSign() {
+  return (
+    <View style={styles.offlineContainer}>
+      <Text style={styles.offlineText}>No Internet Connection</Text>
+    </View>
+  );
+}
+
 class Main extends Component {
 
   componentDidMount = () => {
+    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
     if (!this.props.isGetMainData) {
       return services.getInfo()
         .then(res => {
@@ -25,12 +36,29 @@ class Main extends Component {
     }
   };
 
+  componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  handleConnectivityChange = isConnected => {
+    if (isConnected) {
+      this.props.updateReduxState({ isConnected });
+    } else {
+      this.props.updateReduxState({ isConnected });
+    }
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Map style={styles.map} navigation={this.props.navigation}/>
-      </View>
-    );
+    const { isConnected } = this.props;
+    if (!isConnected) {
+      return <MiniOfflineSign/>;
+    }
+    else
+      return (
+        <View style={styles.container}>
+          <Map style={styles.map} navigation={this.props.navigation}/>
+        </View>
+      );
   }
 }
 
@@ -40,6 +68,7 @@ function mapStateToProps(state) {
     dangers: state.get('dangers'),
     stations: state.get('stations'),
     isGetMainData: state.get('isGetMainData'),
+    isConnected: state.get('isConnected'),
   };
 }
 
@@ -50,5 +79,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  map: {}
+  offlineContainer: {
+    backgroundColor: '#b52424',
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width,
+    position: 'absolute',
+  },
+  offlineText: { color: '#fff' }
 });
