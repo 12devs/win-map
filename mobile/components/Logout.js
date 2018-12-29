82121ac9fb944635fd45cf0739b4a898050e5452
Andrei Yunkevich <andreyha.by@gmail.com>
2018-12-29 14:20:41 +0300

    alert for logout

--- mobile/components/Logout.js
+++ mobile/components/Logout.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import { AsyncStorage } from 'react-native';
+import { Alert, AsyncStorage } from 'react-native';
 import { connect } from "react-redux";
 import actions from "../actions";
 
 class Logout extends Component {
 
   componentDidMount = () => {
-    AsyncStorage.setItem('windToken', '');
-    this.props.updateReduxState({menuRule: 'notLogged', isGetMainData: false});
-    return this.props.navigation.navigate('Login');
+    console.log('componentDidMount');
+    Alert.alert(
+      'Alert',
+      'Do you really want to logout?',
+      [
+        {
+          text: 'No', onPress: () => {
+            console.log('No Pressed');
+            return this.props.navigation.goBack();
+          }, style: 'cancel'
+        },
+        {
+          text: 'Yes', onPress: () => {
+            console.log('Yes Pressed');
+            AsyncStorage.setItem('windToken', '');
+            this.props.updateReduxState({ menuRule: 'notLogged', isGetMainData: false });
+            return this.props.navigation.navigate('Login');
+          }
+        },
+      ],
+      { cancelable: false }
+    );
+
   };
 
   render() {
 
-    return null
+    return null;
   }
 }
 
